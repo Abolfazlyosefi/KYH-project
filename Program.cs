28255c28399c3f690b4157771756f393490682be
Abolfazl Yosefi <abolfazl.yosefi@learnet.se>
2025-01-23 18:07:09 +0100

    initial commit

---
+++ Program.cs
@@ -0,0 +1,25 @@
+var builder = WebApplication.CreateBuilder(args);
+var app = builder.Build();
+
+
+app.MapGet("/", () => "Hello World!");
+
+
+app.MapGet("/add", (int num1, int num2) => AddNumbers(num1, num2));
+
+app.MapGet("/subtract", (int num1, int num2) => SubtractNumbers(num1, num2));
+
+string AddNumbers(int num1, int num2)
+{
+    int sum = num1 + num2;
+    return $"Summan av {num1} och {num2} är: {num1 + num2}";
+
+
+}string SubtractNumbers(int num1, int num2)
+{
+    int sum = num1 + num2;
+    return $"Differencen av {num1} och {num2} är: {num1 - num2}";
+}
+
+
+app.Run();
